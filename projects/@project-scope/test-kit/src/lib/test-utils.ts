aeabdea15c6fbfb47276d30d885dbc3fa3e0ec20
Andres Gesteira <gesteira.pro@gmail.com>
2018-11-04 21:31:03 +0100

    Add test utils.

--- projects/@project-scope/test-kit/src/lib/test-utils.ts
+++ projects/@project-scope/test-kit/src/lib/test-utils.ts
@@ -1,17 +1,26 @@
-import { DebugElement } from '@angular/core';
+import { DebugElement, NgZone } from '@angular/core';
 import { ComponentFixture, tick } from '@angular/core/testing';
 
-/** Wait a tick, then detect changes */
+/**
+ * Insure that we are running functionality inside Angular's zone.
+ * Useful e.g. for router.navigate.
+ */
+export function insureAngularZone(zone: NgZone, fn: () => any): any {
+  return NgZone.isInAngularZone() ? fn() : zone.run(fn);
+}
+
+/**
+ *  Wait a tick, then detect changes
+ */
 export function advance(f: ComponentFixture<any>): void {
   tick();
   f.detectChanges();
 }
 
 /**
- * Create custom DOM event the old fashioned way
- *
- * https://developer.mozilla.org/en-US/docs/Web/API/Event/initEvent
- * Although officially deprecated, some browsers (phantom) don't accept the preferred "new Event(eventName)"
+ * Create custom DOM event the old fashioned way.
+ * Although officially deprecated, some browsers (phantom) don't accept the preferred "new Event(eventName)".
+ * See https://developer.mozilla.org/en-US/docs/Web/API/Event/initEvent
  */
 export function newEvent(eventName: string, bubbles = false, cancelable = false) {
   const evt = document.createEvent('CustomEvent'); // MUST be 'CustomEvent'
@@ -19,14 +28,18 @@ export function newEvent(eventName: string, bubbles = false, cancelable = false)
   return evt;
 }
 
-// See https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
-/** Button events to pass to `DebugElement.triggerEventHandler` for RouterLink event handler */
+/**
+ * Button events to pass to `DebugElement.triggerEventHandler` for RouterLink event handler\
+ * See https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
+ */
 export const ButtonClickEvents = {
   left: { button: 0 },
   right: { button: 2 }
 };
 
-/** Simulate element click. Defaults to mouse left-button click event. */
+/**
+ * Simulate element click. Defaults to mouse left-button click event.
+ */
 export function click(
   el: DebugElement | HTMLElement,
   eventObj: any = ButtonClickEvents.left
